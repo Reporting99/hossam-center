fc4f0e2136e33859ebe7a520997d55d61acf007c
alaa-talab <alaatalab99@gmail.com>
2025-12-08 11:17:29 +0000

    update

--- next.config.js
+++ next.config.js
@@ -7,19 +7,32 @@ module.exports = {
   trailingSlash: SITE.trailingSlash,
   basePath: SITE.basePathname !== '/' ? SITE.basePathname : '',
 
-  swcMinify: true,
+  // Turbopack is default in Next 16; keep empty config to silence warnings
+  turbopack: {},
+  // swcMinify is no longer a valid top-level option in Next 16
   poweredByHeader: false,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'source.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
       },
     ],
-    domains: ['res.cloudinary.com', 'lh3.googleusercontent.com'],
   }
 };
