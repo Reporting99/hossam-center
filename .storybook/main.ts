f5b13398d0c855bcb9731c1fc623528d5d99548a
alaa-talab <reporting@dfeelings.com>
2026-04-23 12:41:00 +0000

    update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,7 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    '@storybook/addon-docs',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
     '@storybook/addon-themes',
